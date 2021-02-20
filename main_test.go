b54d434c7030957a16b5a5d2c002b4eec24f29ad
santiago-rodrig <santo1996.29@gmail.com>
2021-02-20 17:01:34 -0400

    refactor tests to table tests

--- main_test.go
+++ main_test.go
@@ -2,22 +2,28 @@ package awesomeProject
 
 import "testing"
 
+func assertRomanNumeral(t *testing.T, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
 func TestConvertToRoman(t *testing.T) {
-	t.Run("1 gets converted to I", func(t *testing.T) {
-		got := ConvertToRoman(1)
-		want := "I"
+	cases := []struct {
+		Description string
+		Number      int
+		RomanNumber string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
-
-	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(2)
-		want := "II"
-
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Number)
+			assertRomanNumeral(t, got, test.RomanNumber)
+		})
+	}
 }
