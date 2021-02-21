d1aab50a3b9f61d7c21ee874dc172b731064ffd4
santiago-rodrig <santo1996.29@gmail.com>
2021-02-20 23:23:58 -0400

    ConvertToRoman now supports the number 4

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import "strings"
 func ConvertToRoman(number int) string {
 	var result strings.Builder
 
+	if number == 4 {
+		return "IV"
+	}
+
 	for i := 0; i < number; i++ {
 		result.WriteString("I")
 	}
