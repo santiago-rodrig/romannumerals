a50014dd37c937c6ae8382a8b8f0146547eae4cd
santiago-rodrig <santo1996.29@gmail.com>
2021-02-20 23:53:09 -0400

    add support up to 39

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package awesomeProject
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(number int) string {
 	var result strings.Builder
 
-	for number > 0 {
-		switch {
-		case number > 8:
-			result.WriteString("IX")
-			number -= 9
-		case number > 4:
-			result.WriteString("V")
-			number -= 5
-		case number > 3:
-			result.WriteString("IV")
-			number -= 4
-		default:
-			result.WriteString("I")
-			number--
+	for _, numeral := range allRomanNumerals {
+		for number >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			number -= numeral.Value
 		}
 	}
 
