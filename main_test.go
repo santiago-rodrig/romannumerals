35d6b8914a943894c791a62ffaaa05e9b7e58c20
santiago-rodrig <santo1996.29@gmail.com>
2021-02-21 21:13:16 -0400

    complete ConvertToRoman

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package awesomeProject
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func assertRomanNumeral(t *testing.T, got, want string) {
 	t.Helper()
@@ -12,31 +15,42 @@ func assertRomanNumeral(t *testing.T, got, want string) {
 
 func TestConvertToRoman(t *testing.T) {
 	cases := []struct {
-		Description string
 		Number      int
 		RomanNumber string
 	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
-		{"5 gets converted to V", 5, "V"},
-		{"6 gets converted to VI", 6, "VI"},
-		{"7 gets converted to VII", 7, "VII"},
-		{"8 gets converted to VIII", 8, "VIII"},
-		{"9 gets converted to IX", 9, "IX"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-		{"40 gets converted to XL", 40, "XL"},
-		{"47 gets converted to XLVII", 47, "XLVII"},
-		{"49 gets converted to XLIX", 49, "XLIX"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{100, "C"},
+		{90, "XC"},
+		{400, "CD"},
+		{500, "D"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLXXXIV"},
+		{3999, "MMMCMXCIX"},
+		{2014, "MMXIV"},
+		{1006, "MVI"},
+		{798, "DCCXCVIII"},
 	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d should be %q", test.Number, test.RomanNumber), func(t *testing.T) {
 			got := ConvertToRoman(test.Number)
 			assertRomanNumeral(t, got, test.RomanNumber)
 		})
